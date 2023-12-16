6df87da7027991f59bf5bede470d70b56cdd99c5
Yoann <homito-yoann@hotmail.fr>
2023-12-16 09:54:53 +0100

    initRandomBackground uses utils functions

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -18,7 +18,5 @@ function navigateToPage(targetId){
 // ## YOUR ADDED FUNCTIONS ##
 function initRandomBackground(){
     const classes = ['background-01', 'background-02', 'background-03'];
-    const randomIndex = Math.floor(Math.random() * NR_OF_BACKGROUNDS);
-
-    document.body.classList.add(classes[randomIndex]);
+    document.body.classList.add(getRandomListItem(classes));
 }
