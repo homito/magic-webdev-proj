25695f27f68517d627766b348b7e92d903563b3d
Yoann <homito-yoann@hotmail.fr>
2023-12-16 09:51:55 +0100

    + getRandomNumber and getRandomListItem functions

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,11 +1,12 @@
 // ## GIVEN ##
 
 function getRandomNumber(max, min = 0){
-    
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function getRandomListItem(list){
-     
+    const randomIndex = getRandomNumber(list.length - 1);
+    return list[randomIndex];
 }
 
 // ## YOUR ADDED FUNCTIONS ##
