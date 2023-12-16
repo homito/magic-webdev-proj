6e11a5954626bd56aa07837d61f1782fde3f9e08
Yoann <homito-yoann@hotmail.fr>
2023-12-16 09:27:51 +0100

    + initRandomBackground function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,5 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init(){      
-     
+     initNavigation();
 }
--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -3,7 +3,7 @@
 const NR_OF_BACKGROUNDS = 3;
 
 function initNavigation(){
-    
+    initRandomBackground();
 }
 
 function navigate(e){
@@ -16,3 +16,9 @@ function navigateToPage(targetId){
 
 
 // ## YOUR ADDED FUNCTIONS ##
+function initRandomBackground(){
+    const classes = ['background-01', 'background-02', 'background-03'];
+    const randomIndex = Math.floor(Math.random() * NR_OF_BACKGROUNDS);
+
+    document.body.classList.add(classes[randomIndex]);
+}
