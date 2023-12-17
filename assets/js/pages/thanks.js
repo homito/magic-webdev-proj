0ba0a1716b409da02d37ab439cd4750c8c133db5
Yoann <homito-yoann@hotmail.fr>
2023-12-17 10:52:51 +0100

    + dynamic loaded message in thanks page

--- assets/js/pages/thanks.js
+++ assets/js/pages/thanks.js
@@ -4,6 +4,10 @@
 function initThankYouPage(){
     const section = document.querySelector('#thanks');
     navigateToPage(section);
+
+
+    let dynamicHTML = `<p>Good luck and have fun ${subscriptionData.get('name')} with ${config.sets[subscriptionData.get('set')]}!</p>`;
+    section.querySelector('p').innerHTML = dynamicHTML;
 }
 
 // ## YOUR ADDED FUNCTIONS ##
