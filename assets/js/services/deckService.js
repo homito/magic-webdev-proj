43f44b14f9265ca7ed717b8c92d77ffc140f1ff9
Yoann <homito-yoann@hotmail.fr>
2023-12-17 23:19:29 +0100

    fixed logic in getFilteredDeck

--- assets/js/services/deckService.js
+++ assets/js/services/deckService.js
@@ -17,9 +17,12 @@ function getFilteredCardPool(search, types){
 
 // Retrieves a sorted list of cards from the deck, filtered by a search string and types.
 function getFilteredDeck(search, types){
-    if (search === '' || _deck.length === 0)
+    if (_deck.length === 0)
         return _deck;
-    return filterCards(_deck, search, types);
+    else if (search === '')
+        return filterCardsByType(_deck, types);
+    else
+        return filterCards(_deck, search, types);
 }
 
 // Retrieves the complete deck.
