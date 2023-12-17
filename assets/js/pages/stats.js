e72172e869f0fe377ecf58296c81a0e0af70d5d7
Yoann <homito-yoann@hotmail.fr>
2023-12-17 20:02:25 +0100

    +displayOverview

--- assets/js/pages/stats.js
+++ assets/js/pages/stats.js
@@ -5,8 +5,17 @@ function initStatsPage(){
 
     const section = document.querySelector('#stats');
     navigateToPage(section);
+
+    displayOverview();
 }
 
 
 // ## YOUR ADDED FUNCTIONS ##
+function displayOverview() {
+    const p = document.querySelectorAll('#type-stats p');
+    p[0].childNodes[1].textContent = getCreatureCount();
+    p[1].childNodes[1].textContent = getNoneCreatureNoneLandCount();
+    p[2].childNodes[1].textContent = getLandCount();
+    p[3].childNodes[1].textContent = getDeck().length;
+}
 
